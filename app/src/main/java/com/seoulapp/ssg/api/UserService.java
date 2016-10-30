2ac4b17deccb6226cf8bd1d17a05f45268f12e67
BoramMoon <valeur00@gmail.com>
2016-10-30 22:41:45 +0900

    프로필 변경, 다이얼로그 수정 필요git add .git add .git add .!

--- app/src/main/java/com/seoulapp/ssg/api/UserService.java
+++ app/src/main/java/com/seoulapp/ssg/api/UserService.java
@@ -25,4 +25,11 @@ public interface UserService {
     @POST("myProfile")
     Call<User> getMyProfile(@Field("uid") int uid);
 
+    @FormUrlEncoded
+    @POST("send_authMail")
+    Call<User> sendEmail(@Field("uid") int uid, @Field("email") String email);
+
+    @FormUrlEncoded
+    @POST("profile_modify")
+    Call<User> changeEmail(@Field("uid") int uid, @Field("email") String email, @Field("auth_key") String auth_key);
 }
